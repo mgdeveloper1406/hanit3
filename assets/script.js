2e090af420af3b1aefb42476ccd81d3504915a01
Your Name <you@example.com>
2021-04-02 17:59:05 -0400

    updated version

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,25 @@
 var slider = document.getElementById("range");
-var output = document.getElementById("score");
-output.innerHTML = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
+};
+
+// started with the quote API and date display
+var currentTimeEl = document.querySelector("#currentDay");
+currentTimeEl.textContent = moment().format("dddd, MMMM Do YYYY");
+var responseContainerEl = document.querySelector("#api");
+var numRandom = Math.floor(Math.random() * 100);
+function quoteAPI() {
+  fetch("https://type.fit/api/quotes")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      var quote = response[numRandom];
+      var div = document.createElement("div");
+      div.textContent = quote.text;
+      responseContainerEl.append(div);
+    });
 }
+quoteAPI();
+// finished with the quote API and date display
