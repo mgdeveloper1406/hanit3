30e38735da2a7b58d817420092cb555c585d1b24
Michelle <michey.asmah21@gmail.com>
2021-04-10 16:45:20 -0400

    new records retrieval function

--- assets/records.js
+++ assets/records.js
@@ -2,16 +2,28 @@
 var recordsDisplay = document.querySelector("#records-display");
 
 //Function where scores and date will be taken from local storage and displayed on page
-function displayRecords() {
-  var savedRecords = JSON.parse(localStorage.getItem("savedRecords")) || [];
+
+savingRecords = event => {
+
+  setTimeout(function(){ 
+  var savedRecords = JSON.parse(localStorage.getItem("savedRecords")) || []
+
+  event.preventDefault()
 
   //When score is computed save the date and score to local storage
+  
   var scoreRecords = {
     saveDate: localStorage.getItem("date"),
     saveScore: localStorage.getItem("dailyScore")
   };
 
+
   savedRecords.push(scoreRecords);
 
   localStorage.setItem("savedRecords", JSON.stringify(savedRecords))
-}  
+  }, 1000);
+}
+
+
+
+
