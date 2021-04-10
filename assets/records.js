c9d86ab475884654f91742e4c826876edbc68022
Michelle <michey.asmah21@gmail.com>
2021-04-10 16:54:27 -0400

    firstr record display functioning

--- assets/records.js
+++ assets/records.js
@@ -1,5 +1,6 @@
 //Container where scores will be displayed
 var recordsDisplay = document.querySelector("#records-display");
+var savedRecords = JSON.parse(localStorage.getItem("savedRecords")) || []
 
 //Function where scores and date will be taken from local storage and displayed on page
 
@@ -17,12 +18,29 @@ savingRecords = event => {
     saveScore: localStorage.getItem("dailyScore")
   };
 
-
   savedRecords.push(scoreRecords);
+
+  savedRecords.splice(30);
 
   localStorage.setItem("savedRecords", JSON.stringify(savedRecords))
   }, 1000);
 }
+
+//Retrieve scores array from local storage and return each as a success, danger, or neutral card to create the records page
+recordsDisplay.innerHTML = savedRecords.map(record => {
+  if (record.saveScore > 50) {
+    return `                
+    <div class="card border-success m-3 " style="max-width: 18rem;">
+    <div class="card-header">${record.saveDate}</div>
+    <div class="card-body text-success">
+        <h5 class="card-title">${record.saveScore}</h5>
+        <p class="card-text">What a succesful day
+        !</p>
+    </div>
+    </div>`
+  }
+}).join("")
+
 
 
 
